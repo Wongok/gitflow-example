e8876b0ccd17cab9bb221a53387ea79681d6dda4
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-01-20 21:38:13 +0900

    create gitflow

---
+++ src/main/java/git/versioncontrol/gitflow/GitflowApplication.java
@@ -0,0 +1,13 @@
+package git.versioncontrol.gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/git/versioncontrol/gitflow/GitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package git.versioncontrol.gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
